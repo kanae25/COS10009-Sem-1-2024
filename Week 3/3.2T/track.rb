7db86f0fec376360fc6ec5a6991c2d3a3b334073
Kanae <168708128+kanae25@users.noreply.github.com>
2024-11-19 12:57:24 +0700

    Add files via upload

---
+++ Week 3/3.2T/track.rb
@@ -0,0 +1,31 @@
+require "./input_functions"
+
+class Track 
+    attr_accessor :name, :location
+    def initialize(n, l)
+    @name = n
+    @location = l
+    end
+end
+
+def read_track()
+    track_name = read_string("Enter track name:")
+    track_location = read_string("Enter track location:")
+    track = Track.new(track_name, track_location)
+    return track
+end
+
+def print_track(track)
+    puts ("Track name:")
+    puts (track.name)
+    puts ("Track location")
+    puts (track.location)
+end
+
+def main()
+    track = read_track()
+    print_track(track)
+end
+
+
+main() if __FILE__ == $0
